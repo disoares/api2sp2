9232a44f1e2b956e7b13576253892ccc811ca57f
Diego Soares <soaressd01@gmail.com>
2022-12-06 16:58:12 -0300

    Ajustes no .env

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-// Import packages
-const express = require("express");
+const express = require('express');
 const api = require("./routes/api");
 const bodyParser = require("body-parser");
-// Middlewares
+
 const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const cors = require('cors');
-// Routes
 app.use((req, res, next) => {
     console.log("check cors");
     res.header("Access-Control-Allow-Origin", "*")
     app.use(cors);
     next();
 });
+
 app.use("/swap", api);
 app.use("/swapquotein", api);
 app.use("/swapquote", api);
 app.use("/", api);
-// connection
+
 const port = process.env.PORT || 9001;
-app.listen(port, () => console.log(`Listening to port ${port}`));
+app.listen(port, () => {
+    console.log(`Run on port ${port}`);
+});
